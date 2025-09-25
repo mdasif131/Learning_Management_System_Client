3277a2a14663bd9b93b297cfcfc6a42def4c08ba
mdasif131 <asifmdasifchy@gmail.com>
2025-09-25 09:52:22 +0600

    TOTC

--- src/components/homeLayout/index.tsx
+++ src/components/homeLayout/index.tsx
@@ -3,7 +3,7 @@ import Navbar from '@/components/homeLayout/navber/navber';
 import Hero from '@/components/homeLayout/hero/Hero';
 import SeccessSection from '@/components/homeLayout/seccess_Section/seccessSection'
 import CloudFeatures from '@/components/homeLayout/cloud_Features/CloudFeatures';
-import ToTcIntro from './cloud_Features/ToTcIntro';
+import ToTcIntro from './TOTC_intro/ToTcIntro';
 
 
 const HomePage = () => {
